da7f4c67bb421edb042c6dc8177aeba9d6fc500d
mv <madeleine.vercoe@gmail.com>
2023-10-04 14:12:47 +1100

    fix

--- ash-pt/schemas/testimonials.js
+++ ash-pt/schemas/testimonials.js
@@ -1,37 +1,36 @@
-
 const testimonials = {
-    name: 'testimonial',
-    title: 'Testimonial',
-    type: 'document',
-    fields: [
-      {
-        name: 'name',
-        title: 'Name',
-        type: 'string',
-        description: 'The name of the person giving the testimonial.',
+  name: 'testimonial',
+  title: 'Testimonial',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+      description: 'The name of the person giving the testimonial.',
+    },
+    {
+      name: 'yearsTraining',
+      title: 'Years of Training',
+      type: 'number',
+      description: 'The number of years of training the person has undergone.',
+    },
+    {
+      name: 'testimony',
+      title: 'Testimony',
+      type: 'text',
+      description: 'The testimonial content provided by the person.',
+    },
+    {
+      name: 'photo',
+      title: 'Photo',
+      type: 'image',
+      description: 'A photo of the person giving the testimonial.',
+      options: {
+        hotspot: true, // Enables image hotspot for cropping
       },
-      {
-        name: 'yearsTraining',
-        title: 'Years of Training',
-        type: 'number',
-        description: 'The number of years of training the person has undergone.',
-      },
-      {
-        name: 'testimony',
-        title: 'Testimony',
-        type: 'text',
-        description: 'The testimonial content provided by the person.',
-      },
-      {
-        name: 'photo',
-        title: 'Photo',
-        type: 'image',
-        description: 'A photo of the person giving the testimonial.',
-        options: {
-          hotspot: true, // Enables image hotspot for cropping
-        },
-      },
-    ],
-  };
-  
-  export default testimonials
+    },
+  ],
+};
+
+export default testimonials;
